35c0e85cd1e680c080b8c6f0a9d38860391acbee
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-05-20 09:55:27 +0200

    moved the web component from the "bookpreviews.js" to the main file, "scripts.js"

--- scripts.js
+++ scripts.js
@@ -1,5 +1,85 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 
+// created the booksPreview web component
+class booksPreview extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow ({mode: 'open'})
+    }
+  
+    connectedCallback () {
+      const {author, id, image, title} = this.dataset 
+      this.shadowRoot.innerHTML = `
+      <button class="preview" data-preview="${id}">
+         <img class="preview__image" src="${image}"/>
+  
+           <div class="preview__info">
+               <h3 class="preview__title">${title}</h3>
+               <div class="preview__author">${authors[author]}</div>
+           </div></button>
+      
+      <style>
+        .preview {
+                  border-width: 0;
+                  width: 100%;
+                  font-family: Roboto, sans-serif;
+                  padding: 0.5rem 1rem;
+                  display: flex;
+                  align-items: center;
+                  cursor: pointer;
+                  text-align: left;
+                  border-radius: 8px;
+                  border: 1px solid rgba(var(--color-dark), 0.15);
+                  background: rgba(var(--color-light), 1);
+              }
+              
+              @media (min-width: 60rem) {
+                  .preview {
+                  padding: 1rem;
+                  }
+              }
+              
+              .preview_hidden {
+                  display: none;
+              }
+              
+              .preview:hover {
+                  background: rgba(var(--color-blue), 0.05);
+              }
+              
+              .preview__image {
+                  width: 48px;
+                  height: 70px;
+                  object-fit: cover;
+                  background: grey;
+                  border-radius: 2px;
+                  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+                  0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+              }
+              
+              .preview__info {
+                  padding: 1rem;
+              }
+              
+              .preview__title {
+                  margin: 0 0 0.5rem;
+                  font-weight: bold;
+                  display: -webkit-box;
+                  -webkit-line-clamp: 2;
+                  -webkit-box-orient: vertical;  
+                  overflow: hidden;
+                  color: rgba(var(--color-dark), 0.8)
+              }
+              
+              .preview__author {
+                  color: rgba(var(--color-dark), 0.4);
+              }
+      </style>`
+    }
+}
+  
+customElements.define( 'books-preview', booksPreview )
+
 let page = 1;
 let matches = books
 
